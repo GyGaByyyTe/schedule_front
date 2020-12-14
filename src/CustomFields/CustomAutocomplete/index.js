b49c8ecefded8d7928ec5f45a2e5ab0eb5c4313d
Лебедев Андрей Владимирович <lebedev.an.vl@sberbank.ru>
2020-12-14 22:19:47 +0300

    Update autocomplete view

--- src/CustomFields/CustomAutocomplete/index.js
+++ src/CustomFields/CustomAutocomplete/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import {makeStyles} from "@material-ui/core/styles";
-import Chip from "@material-ui/core/Chip";
 
 const useStyles = makeStyles(() => ({
   inputRoot: {
@@ -20,37 +19,20 @@ const CustomAutocomplete = ({ id, model, multiple = false }) => {
   const classes = useStyles();
   const textClasses = useTextStyles();
 
-  return (!multiple
-      ? <Autocomplete
-          id={id}
-          classes={classes}
-          value={model.value}
-          options={model.options}
-          getOptionLabel={(option) => option.title}
-          onChange={(event, newValue) => model.onChange(newValue)}
-          renderInput={(params) => <TextField {...params} classes={textClasses} variant="outlined"/>}
-
-          getOptionSelected={(option, newValue) => option?.value === newValue?.value}
-      />
-      : <Autocomplete
-          id={id}
-          classes={classes}
-          options={model.options}
-          value={model.value}
-          getOptionLabel={(option) => option.title}
-          onChange={(event, newValue) => model.onChange(newValue)}
-          getOptionSelected={(option, newValue) => option?.value === newValue?.value}
-          renderInput={(params) => <TextField{...params} classes={textClasses} variant="outlined"/>}
-          size="small"
-          multiple
-          limitTags={1}
-          renderTags={(value, getTagProps) => null
-              // value.map((option, index) => (
-              //     <Chip variant="outlined" label={option.title} size="small" {...getTagProps({ index })}/>
-              // ))
-          }
-      />)
-
+  return <Autocomplete
+      id={id}
+      classes={classes}
+      value={model.value}
+      options={model.options}
+      getOptionLabel={(option) => option.title}
+      onChange={(event, newValue) => model.onChange(newValue)}
+      getOptionSelected={(option, newValue) => option?.value === newValue?.value}
+      renderInput={(params) => <TextField {...params} classes={textClasses} variant="outlined"/>}
+      size={multiple ? "small" : null}
+      multiple={multiple}
+      limitTags={1}
+      renderTags={(value, getTagProps) => null}
+  />
 }
 
 export default CustomAutocomplete;
